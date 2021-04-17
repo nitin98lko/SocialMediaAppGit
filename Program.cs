6a79fa883078c8caea5c02b6c23274a425195e70
Nitin Roy <nitin98lko@gmail.com>
2021-04-17 17:46:55 +0530

    change msg

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace SocialMediaAppGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello app!");
         }
     }
 }
